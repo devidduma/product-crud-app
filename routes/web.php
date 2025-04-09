18d152fdf9dc6835dd0ece38111623868554b88b
Devid Duma <devidd05@gmail.com>
2025-04-09 16:47:31 +0200

    feat(WebRoute): Routes for mapping CRUD methods on Product with URI

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Returns the homepage with all products
+Route::get('/', ProductController::class .'@index')->name('product.index');
+// Adds a product to the database
+Route::post('/product', ProductController::class .'@store')->name('product.store');
+// Returns a page that shows a full product
+Route::get('/product/{id}', ProductController::class .'@show')->name('product.show');
+// Updates a product
+Route::put('/product/{id}', ProductController::class .'@update')->name('product.update');
+// Deletes a product
+Route::delete('/product/{id}', ProductController::class .'@destroy')->name('product.destroy');
