582734f1fdfc8ecebe9ef013453a2d0c4bf443f7
Devid Duma <devidd05@gmail.com>
2025-04-10 19:37:04 +0200

    feat(WebRoute): Simplify to using Route Resource on Product, for mapping CRUD with URI

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-// Returns the homepage with all products
-Route::get('/', ProductController::class .'@index')->name('product.index');
-// Adds a product to the database
-Route::post('/product', ProductController::class .'@store')->name('product.store');
-// Returns a page that shows a full product
-Route::get('/product/{id}', ProductController::class .'@show')->name('product.show');
-// Updates a product
-Route::put('/product/{id}', ProductController::class .'@update')->name('product.update');
-// Deletes a product
-Route::delete('/product/{id}', ProductController::class .'@destroy')->name('product.destroy');
+Route::resource('product', ProductController::class);
